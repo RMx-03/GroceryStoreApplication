f7662a58b850040645f78056be76cb4edb1a50b3
RMx-03 <140522220+RMx-03@users.noreply.github.com>
2025-12-31 22:03:49 +0530

    Product dao - insert new, delete

--- backend/products_dao.py
+++ backend/products_dao.py
@@ -23,6 +23,28 @@ def get_all_products(connection):
 
     return response
 
+def insert_new_product(connection, product):
+    cursor = connection.cursor()
+    query = ("INSERT INTO products (name, uom_id, price_per_unit) VALUES (%s, %s, %s);")
+    data = (product['name'], product['uom_id'], product['price_per_unit'])
+    cursor.execute(query, data)
+    connection.commit()
+
+    return cursor.lastrowid
+
+def delete_product(connection, product_id):
+    cursor = connection.cursor()
+    query = ("DELETE FROM products where product_id=" + str(product_id))
+    cursor.execute(query)
+    connection.commit()    
+
 if __name__=='__main__':
     connection = get_sql_connection()
+    # print(insert_new_product(connection, {
+    #     'name': 'potatoes',
+    #     'uom_id': '2',
+    #     'price_per_unit': '10'
+    # }))
+    print(get_all_products(connection))
+    print(delete_product(connection, 4))
     print(get_all_products(connection))
